8d1ee78282953112947121e562a6e04780f31979
emilaynerodrigues <emilaynx@gmail.com>
2022-11-11 01:49:17 -0400

    implementando bottom sheet da tela home

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,119 +1,135 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { 
-  StatusBar,
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import {
   View,
-  Text, 
+  Text,
   StyleSheet,
   TouchableOpacity,
   ScrollView,
-  KeyboardAvoidingView
-  } from 'react-native';
+  KeyboardAvoidingView,
+} from "react-native";
 
-import Ionicons from '@expo/vector-icons/Ionicons';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import BottomSheet from "../../Assets/Components/BottomSheet";
+import { Provider } from "react-native-paper";
 
 export default function Home() {
   const navigation = useNavigation();
 
+  const [show, setShow] = useState(false);
+
   return (
-    <KeyboardAvoidingView style={{flex: 1}}>
-    <ScrollView contentContainerStyle={{flex: 1, backgroundColor: "#309F5F"}} showsVerticalScrollIndicator={false}>
-      <StatusBar
-          backgroundColor= "#309F5F"
-          barStyle="light-content"
-        />
+    <Provider style={[{ flex: 1 }]}>
+      <ScrollView
+        contentContainerStyle={{ flex: 1, backgroundColor: "#309F5F" }}
+        showsVerticalScrollIndicator={false}
+      >
         <View style={styles.container}>
-            <View style={styles.greenArea}>
-                <View style={styles.navbar}>
-                  <TouchableOpacity style={styles.changeScreen}>
-                    <Text style={styles.text}>Todos</Text>
-                    <Ionicons name="chevron-down-outline" size={25} color="#fff" />
-                    
-                  </TouchableOpacity>
-                </View>
+        <StatusBar style='auto' />
+          <View style={styles.greenArea}>
+            <View style={styles.navbar}>
+              <TouchableOpacity
+                style={styles.changeScreen}
+                onPress={() => setShow(true)}
+              >
+                <Text style={[styles.text, styles.textWeight]}>Todos</Text>
+                <Ionicons name="chevron-down-outline" size={25} color="#fff" />
+              </TouchableOpacity>
+
+              <BottomSheet
+                show={show}
+                onDismiss={() => {
+                  setShow(false);
+                }}
+                enableBackdropDismiss
+              ></BottomSheet>
+
             </View>
+          </View>
 
-            <View style={styles.flatlistDespesas}>
-
-                <View style={styles.btnPosition}>
-                    <TouchableOpacity style={styles.btnCreate} onPress={ () => navigation.navigate("NewDespesa") }>
-                      <Ionicons name="add" size={30} color="#fff" /> 
-                    </TouchableOpacity>
-                  </View>
+          <View style={styles.flatlistDespesas}>
+            <View style={styles.btnPosition}>
+              <TouchableOpacity
+                style={styles.btnCreate}
+                onPress={() => navigation.navigate("NewDespesa")}
+              >
+                <Ionicons name="add" size={30} color="#fff" />
+              </TouchableOpacity>
             </View>
+          </View>
 
-            <View style={styles.infoArea}>
-                <View style={styles.faltaPagar}>
+          <View style={styles.infoArea}>
+            <View style={styles.faltaPagar}></View>
 
-                </View>
-                
-                <View style={styles.pago}>
-
-                </View>
-            </View>
+            <View style={styles.pago}></View>
+          </View>
         </View>
-    </ScrollView>
-    </KeyboardAvoidingView>
-    );
+      </ScrollView>
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
+    paddingTop: 40
   },
 
-  greenArea:{
+  greenArea: {
     flex: 2,
-    paddingHorizontal: 15
+    paddingHorizontal: 15,
   },
-  
-  changeScreen:{
-    flexDirection: 'row',
-    alignItems: 'center'
+
+  changeScreen: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 
   text: {
     fontSize: 18,
-    fontWeight: '600',
     color: "#fff",
   },
 
-  flatlistDespesas:{
+  textWeight: {
+    fontWeight: "600",
+  },
+
+  flatlistDespesas: {
     flex: 8,
     backgroundColor: "#fff",
     borderTopStartRadius: 20,
-    borderTopEndRadius: 20
+    borderTopEndRadius: 20,
   },
-  
-  btnPosition:{
+
+  btnPosition: {
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "flex-end",
     marginRight: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
-  
-  btnCreate:{
+
+  btnCreate: {
     height: 70,
     width: 70,
     borderRadius: 50,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#309F5F",
-    elevation: 5
+    elevation: 5,
   },
 
-  infoArea:{
-    flex: 1.2
+  infoArea: {
+    flex: 1.2,
   },
 
-  faltaPagar:{
+  faltaPagar: {
     flex: 1,
-    backgroundColor: "#E35D4D"
+    backgroundColor: "#E35D4D",
   },
 
-  pago:{
-    flex: 1
+  pago: {
+    flex: 1,
   },
-})
-
+});
